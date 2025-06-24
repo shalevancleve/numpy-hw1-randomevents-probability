3b17629c2a705648875fa2a0a5015f4b52ef36bf
Shale Van Cleve <shalevancleve@arizona.edu>
2025-06-24 05:44:11 +0000

    new comit

--- hw1.py
+++ hw1.py
@@ -1,14 +1,39 @@
-import numpy as np
+import numpy
 
-np.random.seed(0)
-# 1000 throws of two fair six dice
-throws = np.random.randint(1, 7, size=(1000, 2))
-# Calculate empirical probability of double sixes
-doubleSixes = np.sum((throws[:, 0] == 6) & (throws[:, 1] == 6))
+numpy.random.seed(0)
+throws = numpy.random.randint(1, 7, size=(1000, 2))
+#empirical probability of double sixes
+doubleSixes = numpy.sum((throws[:, 0] == 6) & (throws[:, 1] == 6))
 empiricalProb = doubleSixes / len(throws)
-print("empirical probability of double sixes: ", empiricalProb)
+print("empirical probability of double sixes with 1000 thrown: ", empiricalProb)
 
-moreThrows = np.random.randint(1, 7, size=(10000, 2))
-moreDoubleSixes = np.sum((moreThrows[:, 0] == 6) & (moreThrows[:, 1] == 6))
+moreThrows = numpy.random.randint(1, 7, size=(10000, 2))
+moreDoubleSixes = numpy.sum((moreThrows[:, 0] == 6) & (moreThrows[:, 1] == 6))
 moreEmpiricalProb = moreDoubleSixes / len(moreThrows)
 print("empirical probability of double sixes with 10000 throws: ", moreEmpiricalProb)
+
+#for part 3c
+def computeWinProbability():
+    table = [[0.0 for _ in range(5)] for _ in range(5)]
+
+    #base cases
+    for j in range(4):
+        table[4][j] = 1.0  #win
+    for i in range(4):
+        table[i][4] = 0.0  #loss
+
+    #fill the table -> bottom up
+    for i in reversed(range(4)):
+        for j in reversed(range(4)):
+            if i == j:
+                table[i][j] = 0.5
+            else:
+                table[i][j] = 0.5 * table[i + 1][j] + 0.5 * table[i][j + 1]
+
+    for i in range(4):
+        for j in range(4):
+            print(f"P(W | S=({i}, {j})) = {table[i][j]:.5f}")
+    
+    return table
+
+computeWinProbability()
